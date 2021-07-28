a88f31bc828ecf92f217eae64a9a1c52b8e8f0c8
scoutreilly <scouty33@gmail.com>
2021-07-27 18:48:57 -0600

    first commit: data sqaured away

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+import axios from "axios";
+
+// https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+//
+//
+
 function App() {
+  const [spaceData, setSpaceData] = useState('');
+  const myApiKey = 're1NUI5ffyac6vMsiCG8lSuLGFkwImxhJYwm4dN5';
+
+  useEffect(() => {
+    axios 
+    .get(`https://api.nasa.gov/planetary/apod?api_key=${myApiKey}`)
+    .then(response => setSpaceData(response.data))
+  }, [])
+
+
+  console.log(spaceData);
+
+
+
+
+
+
+
   return (
     <div className="App">
       <p>
